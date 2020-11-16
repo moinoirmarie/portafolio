ca6c086d026e92e63057f74d222a041f837d80ba
Andrea Blasco <moinoirmarie@gmail.com>
2020-11-16 00:01:00 -0500

    modificacion en carrusel de portafolio, ya se ve sobre mi y se oculta el home, tambien se abre el menu, falta que se cierre y dirija a las otras secciones

---
+++ main.js
@@ -0,0 +1,32 @@
+//Variables
+
+const removeClassPerId = (id, clase) => {
+    document.getElementById(id).classList.remove(clase);
+};
+
+const addClassPerId = (id, clase) => {
+    document.getElementById(id).classList.add(clase);
+};
+
+const showAboutScreen = () => {
+    removeClassPerId("aboutScreen", "hidden");
+    addClassPerId("homeScreen", "hidden");
+};
+
+const aboutButton = document.getElementById("aboutBtn");
+aboutButton.addEventListener("click", showAboutScreen);
+
+const showMenu = () => {
+    removeClassPerId("navMenu", "hidden");
+}
+
+const menuButton = document.getElementById("menuBtn");
+menuButton.addEventListener("click", showMenu);
+
+//Instagram Feed
+
+(function(){
+    var i, e, d = document, s = "script";i = d.createElement("script");i.async = 1;
+    i.src = "https://cdn.curator.io/published/3915a838-bab3-441e-bf74-c5278aa124bd.js";
+    e = d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
+})();
